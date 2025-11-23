93ca6933c0a32c9b381ddfb5f3e379ca59bf41e3
Syed Ibrahim <149069626+syedibrahim78@users.noreply.github.com>
2025-11-23 22:52:46 +0530

    Update FCFS.java
    2 sem first program

--- FCFS.java
+++ FCFS.java
@@ -1,5 +1,4 @@
-// CPU SCHEDULING ALGORITHMS IN FCFS
-// (First Come and First Serve)
+
 
 import java.util.*;
 public class FCFS
@@ -9,12 +8,12 @@ public static void main(String args[])
 Scanner sc = new Scanner (System.in);
 System.out.println("enter no of process: ");
 int n = sc.nextInt();
-int pid[] = new int[n]; // process ids
-int ar[] = new int[n]; // arrival times
-int bt[] = new int[n]; // burst or execution times
-int ct[] = new int[n]; // completion times
-int ta[] = new int[n]; // turnaround times
-int wt[] = new int[n]; // waiting times
+int pid[] = new int[n]; 
+int ar[] = new int[n]; 
+int bt[] = new int[n]; 
+int ct[] = new int[n]; 
+int ta[] = new int[n]; 
+int wt[] = new int[n]; 
 int temp;
 float avgwt=0,
 avgta=0;
@@ -45,7 +44,7 @@ pid[j+1] = temp;
 }
 }
 }
-// finding completion times
+
 for(int i = 0 ; i < n; i++)
 {
 if( i == 0)
@@ -61,10 +60,10 @@ ct[i] = ar[i] + bt[i];
 else
 ct[i] = ct[i-1] + bt[i];
 }
-ta[i] = ct[i] - ar[i] ; // turnaround time= completion time- arrival time
-wt[i] = ta[i] - bt[i] ; // waiting time= turnaround time- burst time
-avgwt += wt[i] ; // total waiting time
-avgta += ta[i] ; // total turnaround time
+ta[i] = ct[i] - ar[i] ; 
+wt[i] = ta[i] - bt[i] ; 
+avgwt += wt[i] ; 
+avgta += ta[i] ; 
 }
 System.out.println("\npid arrival brust complete turn waiting");
 for(int i = 0 ; i< n; i++)
@@ -73,8 +72,8 @@ System.out.println(pid[i] + " \t " + ar[i] + "\t" + bt[i] + "\t" + ct[i] + "\t" 
 wt[i] ) ;
 }
 sc.close();
-System.out.println("\naverage waiting time: "+ (avgwt/n)); // printing average waiting
-System.out.println("average turnaround time:"+(avgta/n)); // printing average
+System.out.println("\naverage waiting time: "+ (avgwt/n)); 
+System.out.println("average turnaround time:"+(avgta/n)); 
 
 }
 }
