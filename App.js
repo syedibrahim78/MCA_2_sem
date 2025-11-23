ec01c1a6f12a560cef91568be44a360af527f462
Syed Ibrahim <rajarafic49.gmail.com>
2025-11-23 23:29:36 +0530

    my first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+// const students = []
+
+// function addStudent(name,score){
+//     score = Number(score)
+//     score = score ?? 0;
+//     name = name.trim().toUppercase();
+//     students.push({ name,score})
+
+// }
+
+// // get grades
+
+// function getGrades() {
+//     return students.map((student) => {
+//         let grade = "F"
+
+//         if(student.score >= 90)       grade = "A";
+//         else if(student.score >= 75)  grade = 'B';
+//         else if(student.score >=50)   grade = "C";
+//         else if(student.score >= 35)  grade = "D";
+
+//         return(...student,grade)
+
+
+// })
+    
+// }
+
+
